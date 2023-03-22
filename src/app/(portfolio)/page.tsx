91ec5d3de256a71db5bdc74b1410c8bd8311e19b
Raafa1953 <rafaelajds@gmail.com>
2023-03-22 16:54:10 -0300

    create a motion

--- src/app/(portfolio)/page.tsx
+++ src/app/(portfolio)/page.tsx
@@ -1,4 +1,3 @@
-
 import Header from '../components/sections/Header';
 import Nav from '../components/sections/Nav';
 import About from '../components/sections/About';
@@ -6,17 +5,27 @@ import Experience from '../components/sections/Experience';
 import Portfolio from '../components/sections/Portfolio';
 import Contact from '../components/sections/Contact';
 import Footer from '../components/sections/Footer';
+import WhatsNew from '../components/sections/WhatsNew/WhatsNew';
+import GetStarted from '../components/sections/GetStarted/GetStarted';
 
 export default function Home() {
   return (
-    <div className='container'>
-     <Header />
-      <Nav />
-      <About />
-      <Experience />
-      <Portfolio />
-      <Contact />
-      <Footer />
-    </div>
+    <>
+      <div className="container">
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Portfolio />
+        <Contact />
+
+        <Footer />
+      </div>
+      {/* <div style={{ position: 'relative', display: 'flex' }}>
+        <GetStarted />
+        <div className="gradient-04 z-0" />
+        <WhatsNew />
+      </div> */}
+    </>
   );
 }
